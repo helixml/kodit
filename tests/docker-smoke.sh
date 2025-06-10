c45da1c22b25e11d077cf6293270596799d76bd3
Phil Winder <phil@winder.ai>
2025-06-10 15:53:01 +0100

    try adding some code

--- tests/docker-smoke.sh
+++ tests/docker-smoke.sh
@@ -6,9 +6,18 @@ if [ -z "$TEST_TAG" ]; then
     exit 1
 fi
 
-docker run -i -v $HOME/.kodit:/root/.kodit $TEST_TAG version
-docker run -i -v $HOME/.kodit:/root/.kodit $TEST_TAG index
-docker run -i -v $HOME/.kodit:/root/.kodit $TEST_TAG search keyword "Hello"
-docker run -i -v $HOME/.kodit:/root/.kodit $TEST_TAG search code "Hello"
-docker run -i -v $HOME/.kodit:/root/.kodit $TEST_TAG search hybrid --keywords "main" --code "def main()" --text "main"
-docker run -i -v $HOME/.kodit:/root/.kodit $TEST_TAG serve
+# Create a temporary directory
+tmp_dir=$HOME/tmp/kodit
+mkdir -p $tmp_dir
+
+# Write a dummy python file to the temporary directory
+echo -e "def main():\n    print('Hello, world!')" > $tmp_dir/test.py
+
+
+docker run -i -v $HOME/.kodit:/root/.kodit -v $tmp_dir:/code $TEST_TAG version
+docker run -i -v $HOME/.kodit:/root/.kodit -v $tmp_dir:/code $TEST_TAG index
+docker run -i -v $HOME/.kodit:/root/.kodit -v $tmp_dir:/code $TEST_TAG index /code
+docker run -i -v $HOME/.kodit:/root/.kodit -v $tmp_dir:/code $TEST_TAG search keyword "Hello"
+docker run -i -v $HOME/.kodit:/root/.kodit -v $tmp_dir:/code $TEST_TAG search code "Hello"
+docker run -i -v $HOME/.kodit:/root/.kodit -v $tmp_dir:/code $TEST_TAG search hybrid --keywords "main" --code "def main()" --text "main"
+docker run -i -v $HOME/.kodit:/root/.kodit -v $tmp_dir:/code $TEST_TAG serve
